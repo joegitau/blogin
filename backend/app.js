2b9f4706d85c72d9bbd2ea4b774ee9c2dcd5c0c6
Joe Gitau <joseph.karanja@hotmail.com>
2020-03-29 01:44:37 +0200

    add mongoose + model / schema for posts

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 
@@ -21,5 +22,14 @@ app.use((req, res, next) => {
 const posts = require("./routes/posts");
 
 app.use("/api/posts", posts);
+
+mongoose
+  .connect("mongodb://localhost/blogin", {
+    useFindAndModify: false,
+    useUnifiedTopology: false,
+    useNewUrlParser: false
+  })
+  .then(() => console.log("Connected to Mongodb..."))
+  .catch(err => console.error("could not connect to mongodb...", err));
 
 module.exports = app;
